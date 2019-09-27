6212043d5cd876a529ce8adb7f6b23134d96a076
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-09-27 16:41:11 -0400

    added the review_date to the review table and implemented a few review method

--- SoloProject/src/com/fdmgroup/dao/implementation/ReviewDAO.java
+++ SoloProject/src/com/fdmgroup/dao/implementation/ReviewDAO.java
@@ -1,30 +1,63 @@
 package com.fdmgroup.dao.implementation;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
 
 import com.fdmgroup.dao.interfaces.IReviewDAO;
 import com.fdmgroup.model.Review;
 import com.fdmgroup.model.User;
+import com.fdmgroup.util.DataSource;
 
 ////////////////// BONUS ///////////////////////
-public class ReviewDAO implements IReviewDAO{
+public class ReviewDAO implements IReviewDAO {
 
 	@Override
 	public boolean addReview(Review b) {
 		return false;
-		
-	}
 
-	@Override
+	}
+	
 	public ArrayList<Review> retrieveReviews(int productID) {
-		// TODO Auto-generated method stub
-		return null;
+		String query = "select * from review where product_id=?";
+		ArrayList<Review> retreiver = new ArrayList<>();
+		try (Connection con = DataSource.getInstance().getConnection();
+				PreparedStatement stmt = con.prepareStatement(query);) {
+			stmt.setInt(1, productID);
+			ResultSet rs = stmt.executeQuery();
+			
+			while(rs.next()) {
+				String emailAddress = rs.getString("email_address");
+				int rating = rs.getInt("rating");
+				String reviewText = rs.getString("review_text");
+				int reviewID = rs.getInt("review_id");
+				Timestamp time = rs.getTimestamp("review_date");
+				retreiver.add(new Review(reviewText,emailAddress,reviewID,productID,rating,time));
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return retreiver;
 	}
 
-	@Override
 	public boolean removeReview(User u, int productID) {
-		// TODO Auto-generated method stub
-		return false;
+		String query = "delete from review where email_address=? and product_id=?";
+		
+		try (Connection con = DataSource.getInstance().getConnection();
+				PreparedStatement stmt = con.prepareStatement(query);) {
+			stmt.setString(1, u.getUsername());
+			stmt.setInt(2, productID);
+			stmt.execute();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 
 	@Override
