860b2847161eb701bff02486290428109a37f9cf
Saeeds28 <awesomesaad.saeed2@gmail.com>
2019-10-03 11:14:18 -0400

    address dao ported successfully to JPA

--- SoloProject/src/com/fdmgroup/app/MainApp.java
+++ SoloProject/src/com/fdmgroup/app/MainApp.java
@@ -13,21 +13,21 @@ import com.fdmgroup.view.HomeView;
 public class MainApp {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		EntityManagerFactory emf;
-		EntityManager em;
-		emf = Persistence.createEntityManagerFactory("SoloProject");
-		em = emf.createEntityManager();
-		Address a = new Address("saeeds28", "130 Adelaide W", "Toronto", "Ontaio", "Canada", "M5H3P5");
-		User u = new User("saeeds28", "bluejays123", "Saad", "Saeed", "admin", a);
-
-		em.getTransaction().begin();
-		em.persist(u);
-		//em.persist(a);
-		em.getTransaction().commit();
-////		
-		em.close();
-		emf.close();
-		
+//		EntityManagerFactory emf;
+//		EntityManager em;
+//		emf = Persistence.createEntityManagerFactory("SoloProject");
+//		em = emf.createEntityManager();
+//		Address a = new Address("saeeds28", "130 Adelaide W", "Toronto", "Ontaio", "Canada", "M5H3P5");
+//		User u = new User("saeeds28", "bluejays123", "Saad", "Saeed", "admin", a);
+//
+//		em.getTransaction().begin();
+//		em.persist(u);
+//		//em.persist(a);
+//		em.getTransaction().commit();
+//////		
+//		em.close();
+//		emf.close();
+//		
 		//Views
 		HomeView hv = new HomeView(scanner);
 		hv.showInitialOptions(false);
--- SoloProject/src/com/fdmgroup/dao/implementation/AddressDAO.java
+++ SoloProject/src/com/fdmgroup/dao/implementation/AddressDAO.java
@@ -1,10 +1,5 @@
 package com.fdmgroup.dao.implementation;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -12,8 +7,7 @@ import javax.persistence.Persistence;
 import com.fdmgroup.dao.interfaces.IAddressDAO;
 import com.fdmgroup.model.Address;
 import com.fdmgroup.model.User;
-import com.fdmgroup.model.UserSession;
-import com.fdmgroup.util.DataSource;
+
 
 public class AddressDAO implements IAddressDAO {
 
@@ -26,78 +20,37 @@ public class AddressDAO implements IAddressDAO {
 	}
 
 	public boolean changeCity(String city, User u) {
-		String query = "UPDATE address " + "SET city = ?" + "Where email_address = ? ";
-		try (Connection con = DataSource.getInstance().getConnection();
-				PreparedStatement stmt = con.prepareStatement(query);) {
-			stmt.setString(1, city);
-			stmt.setString(2, u.getUsername());
-			stmt.executeUpdate();
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return false;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
+		Address a = em.find(Address.class, u.getUsername());
+		em.getTransaction().begin();
+		a.setCity(city);
+		em.getTransaction().commit();
 		return true;
 	}
 
 	public boolean changeStreet(String street, User u) {
-		String query = "UPDATE address " + "SET street = ?" + "Where email_address = ? ";
-		try (Connection con = DataSource.getInstance().getConnection();
-				PreparedStatement stmt = con.prepareStatement(query);) {
-			stmt.setString(1, street);
-			stmt.setString(2, u.getUsername());
-			stmt.executeUpdate();
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return false;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
+		Address a = em.find(Address.class, u.getUsername());
+		
+		em.getTransaction().begin();
+		a.setStreet(street);
+		em.getTransaction().commit();
 		return true;
 	}
 
 	public boolean changeCountry(String country, User u) {
-		String query = "UPDATE address " + "SET country = ?" + "Where email_address = ? ";
-		try (Connection con = DataSource.getInstance().getConnection();
-				PreparedStatement stmt = con.prepareStatement(query);) {
-			stmt.setString(1, country);
-			stmt.setString(2, u.getUsername());
-			stmt.executeUpdate();
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return false;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
+		Address a = em.find(Address.class, u.getUsername());
+		
+		em.getTransaction().begin();
+		a.setCountry(country);
+		em.getTransaction().commit();
 		return true;
 	}
 
 	public boolean changePostalCode(String postal, User u) {
-		String query = "UPDATE address " + "SET postal_code= ?" + "Where email_address = ? ";
-		try (Connection con = DataSource.getInstance().getConnection();
-				PreparedStatement stmt = con.prepareStatement(query);) {
-			stmt.setString(1, postal);
-			stmt.setString(2, u.getUsername());
-			stmt.executeUpdate();
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return false;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
+		Address a = em.find(Address.class, u.getUsername());
+		
+		em.getTransaction().begin();
+		a.setPostalCode(postal);
+		em.getTransaction().commit();
 		return true;
 	}
 
@@ -107,30 +60,12 @@ public class AddressDAO implements IAddressDAO {
 	}
 
 	public boolean changeProvince(String province, User u) {
-		String query = "UPDATE address " + "SET province = ?" + "Where email_address = ? ";
-		try (Connection con = DataSource.getInstance().getConnection();
-				PreparedStatement stmt = con.prepareStatement(query);) {
-			stmt.setString(1, province);
-			stmt.setString(2, u.getUsername());
-			stmt.executeUpdate();
-
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return false;
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
+		Address a = em.find(Address.class, u.getUsername());
+		
+		em.getTransaction().begin();
+		a.setProvince(province);
+		em.getTransaction().commit();
 		return true;
 	}
 
-//	public boolean addAddress(Address a) {
-//		User u = em.find(User.class, UserSession.getLoggedInUser().getUsername());
-//		em.getTransaction().begin();
-//		u.setAddress(a);
-//		em.merge(u);
-//		em.getTransaction().commit();
-//		return true;
-//	}
 }
