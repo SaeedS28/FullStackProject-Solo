0e7d6d1121fe390dd79a730852e84fd4f3ea2954
SaeedS28 <awesomesaad.saeed2@gmail.com>
2019-10-18 12:51:39 -0400

    need to work on the servlets

--- ShopForAll/src/com/fdmgroup/app/MainApp.java
+++ ShopForAll/src/com/fdmgroup/app/MainApp.java
@@ -16,48 +16,48 @@ import com.fdmgroup.view.HomeView;
 public class MainApp {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		EntityManagerFactory emf;
-		EntityManager em;
-		emf = Persistence.createEntityManagerFactory("SoloProject");
-		em = emf.createEntityManager();
-
-		em.getTransaction().begin();
-		Address a = new Address("saeeds28", "130 Adelaide W", "Toronto", "Ontaio", "Canada", "M5H3P5");
-		Address a2 = new Address("samad", "25 Telegram Mews", "Toronto", "Ontaio", "Canada", "M5V3Z1");
-		
-		// Users
-		User u = new User("saeeds28", "bluejays123", "Saad", "Saeed", "admin", a);
-		User u2 = new User("samad", "password", "Samad", "Saeed", "regular", a2);
-
-		List<Item> strawberry = new ArrayList<>();
-		strawberry.add(new Item("Boots", "Clothing", "They cover your feet from the cold and wet", 32, 12.99));
-		strawberry.add( new Item("Google Pixel Phone", "Technology", "It's a phone fam", 12, 699.99));
-		strawberry.add(new Item("Water Bottle", "Essentials", "You can smuggle your alcohol now", 42, 5.99));
-		strawberry.add( new Item("Baseballs", "Sports", "You can now bash people with 95 mph fastball", 32, 12.99));
-		strawberry.add(new Item("Glasses", "Health", "Make blind people see", 45, 199.99));
-		strawberry.add( new Item("Surface Pro 7", "Technology", "Latest and greatest computer so far", 12, 2099.99));
-		strawberry.add( new Item("Chocolate Bars", "Food/Beverage", "Dan will eat these in a flash", 85, 3.99));
-		strawberry.add(new Item("Mango Pudding", "Food/Beverage", "Dan will defintely eat these in a heartbeat", 50, 0.99));
-		strawberry.add(new Item("Jackets", "Clothing", "Leather jacket to keep you warm", 30, 69.99));
-		strawberry.add(new Item("Laniard", "Clothing", "Good for keeping your IDs in view", 32, 8.99));
-		strawberry.add(new Item("Wooden Table", "Furniture", "It's a table made of dark maple wood", 32, 69.69));
-		strawberry.add( new Item("We are not your kind", "Music", "Slipknot's latest album. It's dank", 32, 39.99));
-		strawberry.add( new Item("Lightbulb", "Utility", "Only one person required to screw this one in", 72, 2.99));
-		strawberry.add( new Item("Pirated Movies", "Illegal", "Assorted bunch to choose from", 420, 2.99));
-		strawberry.add( new Item("Spiked Baseball bats", "Illegal", "We are not liable for any damage. Oh, they pop heads.", 32, 42.99));
-		strawberry.add( new Item("Pizza", "Food/Beverage", "Something definitely ain't right here", 45, 12.99));
-		
-		em.persist(u);
-		em.persist(u2);
-		
-		for (Item i : strawberry){
-			em.persist(i);
-		}
-		
-		em.getTransaction().commit();
-////		
-		em.close();
-		emf.close();
+//		EntityManagerFactory emf;
+//		EntityManager em;
+//		emf = Persistence.createEntityManagerFactory("SoloProject");
+//		em = emf.createEntityManager();
+//
+//		em.getTransaction().begin();
+//		Address a = new Address("saeeds28", "130 Adelaide W", "Toronto", "Ontaio", "Canada", "M5H3P5");
+//		Address a2 = new Address("samad", "25 Telegram Mews", "Toronto", "Ontaio", "Canada", "M5V3Z1");
+//		
+//		// Users
+//		User u = new User("saeeds28", "bluejays123", "Saad", "Saeed", "admin", a);
+//		User u2 = new User("samad", "password", "Samad", "Saeed", "regular", a2);
+//
+//		List<Item> strawberry = new ArrayList<>();
+//		strawberry.add(new Item("Boots", "Clothing", "They cover your feet from the cold and wet", 32, 12.99));
+//		strawberry.add( new Item("Google Pixel Phone", "Technology", "It's a phone fam", 12, 699.99));
+//		strawberry.add(new Item("Water Bottle", "Essentials", "You can smuggle your alcohol now", 42, 5.99));
+//		strawberry.add( new Item("Baseballs", "Sports", "You can now bash people with 95 mph fastball", 32, 12.99));
+//		strawberry.add(new Item("Glasses", "Health", "Make blind people see", 45, 199.99));
+//		strawberry.add( new Item("Surface Pro 7", "Technology", "Latest and greatest computer so far", 12, 2099.99));
+//		strawberry.add( new Item("Chocolate Bars", "Food/Beverage", "Dan will eat these in a flash", 85, 3.99));
+//		strawberry.add(new Item("Mango Pudding", "Food/Beverage", "Dan will defintely eat these in a heartbeat", 50, 0.99));
+//		strawberry.add(new Item("Jackets", "Clothing", "Leather jacket to keep you warm", 30, 69.99));
+//		strawberry.add(new Item("Laniard", "Clothing", "Good for keeping your IDs in view", 32, 8.99));
+//		strawberry.add(new Item("Wooden Table", "Furniture", "It's a table made of dark maple wood", 32, 69.69));
+//		strawberry.add( new Item("We are not your kind", "Music", "Slipknot's latest album. It's dank", 32, 39.99));
+//		strawberry.add( new Item("Lightbulb", "Utility", "Only one person required to screw this one in", 72, 2.99));
+//		strawberry.add( new Item("Pirated Movies", "Illegal", "Assorted bunch to choose from", 420, 2.99));
+//		strawberry.add( new Item("Spiked Baseball bats", "Illegal", "We are not liable for any damage. Oh, they pop heads.", 32, 42.99));
+//		strawberry.add( new Item("Pizza", "Food/Beverage", "Something definitely ain't right here", 45, 12.99));
+//		
+//		em.persist(u);
+//		em.persist(u2);
+//		
+//		for (Item i : strawberry){
+//			em.persist(i);
+//		}
+//		
+//		em.getTransaction().commit();
+//////		
+//		em.close();
+//		emf.close();
 
 		//Views
 		HomeView hv = new HomeView(scanner);
