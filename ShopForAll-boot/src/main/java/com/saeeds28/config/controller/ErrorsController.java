26d947cd3736db883e8dd3c220b314cabe1e5133
Saad Saeed <awesomesaad.saeed2@gmail.com>
2020-06-07 18:24:11 -0400

    custom error page shows up when a 404 status is encountered

---
+++ ShopForAll-boot/src/main/java/com/saeeds28/config/controller/ErrorsController.java
@@ -0,0 +1,37 @@
+package com.saeeds28.config.controller;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ErrorsController implements ErrorController {
+
+	private static final String PATH= "/error";
+	
+	@RequestMapping(method = RequestMethod.GET, path = PATH)
+	public ModelAndView handleError(HttpServletRequest request) {
+		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+		
+		if(status != null) {
+			Integer code = Integer.valueOf(status.toString());
+			
+			if(code == HttpStatus.NOT_FOUND.value()) {
+				return new ModelAndView("Status404");
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public String getErrorPath() {
+		// TODO Auto-generated method stub
+		return PATH;
+	}
+}
